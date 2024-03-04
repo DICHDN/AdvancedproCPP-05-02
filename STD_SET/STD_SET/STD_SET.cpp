d1d5d0b6a9ff6038ba6e96383556ffc8ff0732ed
DICHDN <146694203+DICHND-CPP@users.noreply.github.com>
2024-03-04 07:15:41 +0300

    first commit

---
+++ STD_SET/STD_SET/STD_SET.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <set>
+
+
+
+int main()
+{
+    std::set<int> s1{80,40,90,30,45,87,110,20,35,42,80,85,88,100,120};
+    for (const auto& el : s1)
+    {
+        std::cout << el << " ";
+    }
+    std::cout << std::endl;
+    //s1.erase(s1.begin(), s1.lower_bound(45));
+    s1.erase(s1.begin(), s1.lower_bound(90));
+    for (const auto& el : s1)
+    {
+        std::cout << el << " ";
+    }
+    std::cout << std::endl;
+    
+
+}
+
---
+++ Template class/Template class/Template class.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+class table
+{
+protected:
+    table** matrix = nullptr;
+    int stb = 0;
+    int str = 0;
+public:
+    table(int stb, int str)
+    {
+        this->stb = stb;
+        this->str = str;
+    }
+    table **matrix0(int stb, int str)
+    {
+        this->stb = stb;
+        this->str = str;
+        table** matrix1 = new T * [stb];
+        for (int i = 0;i < stb; ++i)
+        {
+            matrix1[i] = new T[str];
+        }
+        
+        for (int i = 0; i < stb; ++i)
+        {
+            for (int j = 0; j < stb; ++j)
+            {
+                matrix1[i][j] = NULL;
+            }
+        }
+        this->matrix = matrix1;
+            return matrix1;
+    }
+    
+    T get_element(int stb,int str) const
+    {
+        return matrix[stb][str];
+    }
+    T set_element(int stb, int str, T element)
+    {
+        
+        matrix[stb][str] = element; //требуется перегрузка?
+        
+    }
+    ~table()
+    {
+        for (int i = 0; i < stb; ++i)
+        {
+            delete matrix[i];
+        }
+        delete[] matrix;
+    }
+
+};
+    
+          
+
+
+int main()
+{
+    table<double> a(5,5);
+    a.set_element(0, 0, 4);
+    std::cout << a.get_element(0,0);
+}
+
